e191490bfe1eddb137fd91ab571e5fde77b5ffa0
Alexander Samokhin <samokhin@shocklamd.ru>
2020-10-14 13:11:55 +0300

    stage2 alpha

--- stage2.py
+++ stage2.py
@@ -34,6 +34,18 @@ from PIL import ImageOps, ImageFilter
 
 # # Основные настройки
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
 # Batch size
 BATCH_SIZE           = 8 # уменьшаем batch если сеть большая, иначе не влезет в память на GPU
 BATCH_SIZE_STEP4     = 2 # Размер batch на шаге с увеличенными изображениями
@@ -71,24 +83,95 @@ input_shape          = (IMG_SIZE, IMG_SIZE, IMG_CHANNELS)
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)  
 PYTHONHASHSEED = 0
-
 DATA_PATH = '/home/alex/Car_Classification/input/'
 
-keras.models.load_model('../working/model_step3.hdf5')
+sample_submission = pd.read_csv(DATA_PATH+"sample-submission.csv")
+
+model = keras.models.load_model('../working/model_step3.hdf5')
 model.load_weights('best_model.hdf5')
-
-
-scores = model.evaluate(test_generator, verbose=1)
-print("Accuracy: %.2f%%" % (scores[1]*100))
-
 
 # ## Step - увеличение размера изображения
 
 # Увеличим размер изображения и уменьшим уровень аугментации
 
-
 input_shape_step4 = (IMG_SIZE_STEP4, IMG_SIZE_STEP4, IMG_CHANNELS)
 
+class CyclicLR(keras.callbacks.Callback):
+    
+    def __init__(self,base_lr, max_lr, step_size, base_m, max_m, cyclical_momentum):
+ 
+        self.base_lr = base_lr
+        self.max_lr = max_lr
+        self.base_m = base_m
+        self.max_m = max_m
+        self.cyclical_momentum = cyclical_momentum
+        self.step_size = step_size
+        
+        self.clr_iterations = 0.
+        self.cm_iterations = 0.
+        self.trn_iterations = 0.
+        self.history = {}
+        
+    def clr(self):
+        
+        cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
+        
+        if cycle == 2:
+            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)          
+            return self.base_lr-(self.base_lr-self.base_lr/100)*np.maximum(0,(1-x))
+        
+        else:
+            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
+            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0,(1-x))
+    
+    def cm(self):
+        
+        cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
+        
+        if cycle == 2:
+            
+            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1) 
+            return self.max_m
+        
+        else:
+            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
+            return self.max_m - (self.max_m-self.base_m)*np.maximum(0,(1-x))
+        
+        
+    def on_train_begin(self, logs={}):
+        logs = logs or {}
+
+        if self.clr_iterations == 0:
+            K.set_value(self.model.optimizer.lr, self.base_lr)
+        else:
+            K.set_value(self.model.optimizer.lr, self.clr())
+            
+        if self.cyclical_momentum == True:
+            if self.clr_iterations == 0:
+                K.set_value(self.model.optimizer.momentum, self.cm())
+            else:
+                K.set_value(self.model.optimizer.momentum, self.cm())
+            
+            
+    def on_batch_begin(self, batch, logs=None):
+        
+        logs = logs or {}
+        self.trn_iterations += 1
+        self.clr_iterations += 1
+
+        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
+        self.history.setdefault('iterations', []).append(self.trn_iterations)
+        
+        if self.cyclical_momentum == True:
+            self.history.setdefault('momentum', []).append(K.get_value(self.model.optimizer.momentum))
+
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+        
+        K.set_value(self.model.optimizer.lr, self.clr())
+        
+        if self.cyclical_momentum == True:
+            K.set_value(self.model.optimizer.momentum, self.cm())
 
 
 AUGMENTATIONS = albumentations.Compose([
@@ -141,26 +224,22 @@ test_generator = train_datagen.flow_from_directory(
 # Заново создаем сеть с новым размером входных данных
 
 
-base_model = efn.EfficientNetB6(weights='imagenet', include_top=False, input_shape=input_shape)
-
-
-
 # Настройки
 batch_size = BATCH_SIZE_STEP4
 epochs = EPOCHS_STEP4
 base_lr = LR_STEP4
 max_lr = base_lr*10
-#max_m = MAX_MOMENTUM
-#base_m = BASE_MOMENTUM
+max_m = MAX_MOMENTUM
+base_m = BASE_MOMENTUM
 
-#cyclical_momentum = CYCLICAL_MOMENTUM
-#augment = AUGMENT
-#cycles = CYCLES
+cyclical_momentum = CYCLICAL_MOMENTUM
+augment = AUGMENT
+cycles = CYCLES
 
 # Расчет количества итерация и шага изменения learning rate
 iterations = round(train_generator.samples//train_generator.batch_size*epochs)
 iterations = list(range(0,iterations+1))
-step_size = len(iterations)/(cycles)
+step_size = len(iterations)/(CYCLES)
 
 model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=base_lr, momentum=BASE_MOMENTUM), metrics=["accuracy"])
 
@@ -176,16 +255,16 @@ clr =  CyclicLR(
 )
     
 # Добавим ModelCheckpoint чтоб сохранять прогресс обучения модели и можно было потом подгрузить и дообучить модель.    
-#checkpoint = ModelCheckpoint('best_model.hdf5' , monitor = ['val_accuracy'] , verbose = 1  , mode = 'max')
-#earlystop = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
+checkpoint = ModelCheckpoint('best_model.hdf5' , monitor = ['val_accuracy'] , verbose = 1  , mode = 'max')
+earlystop = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
 callbacks_list = [checkpoint, earlystop, clr]
 
 # Обучаем
-history = model.fit_generator(
+history = model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples//train_generator.batch_size,
     validation_data = test_generator, 
-    validation_steps = train_generator.samples//train_generator.batch_size,
+    validation_steps = test_generator.samples//test_generator.batch_size,
     epochs = epochs,
     callbacks = callbacks_list
 )
