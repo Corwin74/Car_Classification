0a0568cc0ab94910e47696e1bf935e7c8dce7f34
Alexander Samokhin <samokhin@shockland.ru>
2020-10-14 09:55:28 +0300

    stage2

--- stage2.py
+++ stage2.py
@@ -1,12 +1,3 @@
-# # Классификация изображений
-# 
-# ### Основная идея этого решения: взять предобученую на ImageNet сеть EfficientNetB6 и дообучить под нашу задачу. 
-# 
-# Также в ноутбуке будут использоваться такие приемы, как:
-# * аугментация при помощи библиотеке albumentations
-# * политика One Cycle Policy для Learning Rate
-# * аугментация изображений из тестовой выборки - test time augmentation (TTA)
-
 # # Установка и импорт необходимых библиотек
 
 import numpy as np
@@ -40,31 +31,6 @@ from sklearn.model_selection import train_test_split
 
 import PIL
 from PIL import ImageOps, ImageFilter
-#увеличим дефолтный размер графиков
-from pylab import rcParams
-rcParams['figure.figsize'] = 10, 5
-#графики в svg выглядят более четкими
-get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-#print(os.listdir("../input"))
-print('Python       :', sys.version.split('\n')[0])
-print('Numpy        :', np.__version__)
-print('Tensorflow   :', tf.__version__)
-print('Keras        :', keras.__version__)
-
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-  try:
-    # Currently, memory growth needs to be the same across GPUs
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-  except RuntimeError as e:
-    # Memory growth must be set before GPUs have been initialized
-    print(e)
 
 # # Основные настройки
 
@@ -73,9 +39,9 @@ BATCH_SIZE           = 8 # уменьшаем batch если сеть больш
 BATCH_SIZE_STEP4     = 2 # Размер batch на шаге с увеличенными изображениями
 
 # Epochs
-EPOCHS_STEP1         = 10  
-EPOCHS_STEP2         = 10  
-EPOCHS_STEP3         = 10   
+EPOCHS_STEP1         = 20  
+EPOCHS_STEP2         = 20  
+EPOCHS_STEP3         = 20   
 EPOCHS_STEP4         = 7  
 
 # Learning Rates
@@ -95,9 +61,9 @@ CYCLES = 2.35
 VAL_SPLIT            = 0.2 # сколько данных выделяем на тест = 20%
 
 CLASS_NUM            = 10  # количество классов в нашей задаче
+
 IMG_SIZE             = 250 # какого размера подаем изображения в сеть
 IMG_SIZE_STEP4       = 512
-
 IMG_CHANNELS         = 3   # у RGB 3 канала
 input_shape          = (IMG_SIZE, IMG_SIZE, IMG_CHANNELS)
 
@@ -108,365 +74,11 @@ PYTHONHASHSEED = 0
 
 DATA_PATH = '/home/alex/Car_Classification/input/'
 
-# EDA / Анализ данных
+keras.models.load_model('../working/model_step3.hdf5')
+model.load_weights('best_model.hdf5')
 
-df = pd.read_csv(DATA_PATH + "train.csv")
-sample_submission = pd.read_csv(DATA_PATH + "sample-submission.csv")
-df.head()
-df.info()
-
-# Аугментация с использованием библиотеки Albumentations
-
-AUGMENTATIONS = albumentations.Compose([
-    albumentations.HorizontalFlip(p=0.5),
-    albumentations.Rotate(limit=30, interpolation=1, border_mode=4, value=None, mask_value=None, always_apply=False, p=0.5),
-    albumentations.OneOf([
-        albumentations.CenterCrop(height=250, width=200),
-        albumentations.CenterCrop(height=200, width=250),
-    ],p=0.5),
-    albumentations.OneOf([
-        albumentations.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3),
-        albumentations.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.1)
-    ],p=0.5),
-    albumentations.GaussianBlur(p=0.05),
-    albumentations.HueSaturationValue(p=0.5),
-    albumentations.RGBShift(p=0.5),
-    albumentations.FancyPCA(alpha=0.1, always_apply=False, p=0.5),
-    albumentations.Resize(IMG_SIZE, IMG_SIZE)
-])
-
-train_datagen = ImageDataAugmentor(
-        rescale=1./255,
-        augment = AUGMENTATIONS,
-        validation_split=VAL_SPLIT,
-        )
-        
-test_datagen = ImageDataAugmentor(rescale=1./255)
-
-# Генерация данных
-
-# Завернем наши данные в генератор:
-
-train_generator = train_datagen.flow_from_directory(
-    PATH+'train/',      # директория где расположены папки с картинками 
-    target_size=(IMG_SIZE, IMG_SIZE),
-    batch_size=BATCH_SIZE,
-    class_mode='categorical',
-    shuffle=True, seed=RANDOM_SEED,
-    subset='training') # set as training data
-
-test_generator = train_datagen.flow_from_directory(
-    PATH+'train/',
-    target_size=(IMG_SIZE, IMG_SIZE),
-    batch_size=BATCH_SIZE,
-    class_mode='categorical',
-    shuffle=True, seed=RANDOM_SEED,
-    subset='validation') # set as validation data
-
-
-
-# # Построение модели
-
-# На момент выполнения задания сеть EfficientNetB6 все ещё является оптимальным выбором, 
-# если брать во внимание точность предсказаний и время работы.
-
-# Загружаем предобученную сеть EfficientNetB6:
-
-
-base_model = efn.EfficientNetB6(
-    weights='imagenet', # Подгружаем веса imagenet
-    include_top=False,  # Выходной слой (голову) будем менять т.к. у нас другие классы
-    input_shape=input_shape)
-
-
-# Для начала заморозим веса EfficientNetB6 и обучим только "голову". 
-# Делаем это для того, чтобы хорошо обученные признаки на Imagenet не затирались в самом начале нашего обучения
-base_model.trainable = False
-
-# Устанавливаем "голову"
-
-model=M.Sequential()
-model.add(base_model)
-model.add(L.GlobalAveragePooling2D(),) # объединяем все признаки в единый вектор 
-
-# Экспериментируем с архитектурой - добавляем ещё один полносвязный слой, dropout и batch-нормализацию
-
-model.add(L.Dense(256, activation='relu'))
-model.add(L.BatchNormalization())
-model.add(L.Dropout(0.25))
-model.add(L.Dense(CLASS_NUM, activation='softmax'))
-
-# Количество слоев
-print(len(model.layers))
-
-# Количество параметров обучения
-len(model.trainable_variables)
-
-# Статус слоев - будем обучать или нет
-for layer in model.layers:
-    print(layer, layer.trainable)
-
-
-# # Обучение модели
-
-# ## Управление Learning Rate - One Cycle Policy
-
-# Реализуем one cycle policy. Код взят отсюда: https://www.kaggle.com/robotdreams/one-cycle-policy-with-keras
-
-# Implement One Cycle Policy Algorithm in the Keras Callback Class
-
-from sklearn.metrics import log_loss, roc_auc_score, accuracy_score
-from keras.losses import binary_crossentropy
-from keras.metrics import binary_accuracy
-from keras import backend as K
-from keras.callbacks import *
-
-class CyclicLR(keras.callbacks.Callback):
-    
-    def __init__(self,base_lr, max_lr, step_size, base_m, max_m, cyclical_momentum):
- 
-        self.base_lr = base_lr
-        self.max_lr = max_lr
-        self.base_m = base_m
-        self.max_m = max_m
-        self.cyclical_momentum = cyclical_momentum
-        self.step_size = step_size
-        
-        self.clr_iterations = 0.
-        self.cm_iterations = 0.
-        self.trn_iterations = 0.
-        self.history = {}
-        
-    def clr(self):
-        
-        cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
-        
-        if cycle == 2:
-            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)          
-            return self.base_lr-(self.base_lr-self.base_lr/100)*np.maximum(0,(1-x))
-        
-        else:
-            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
-            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0,(1-x))
-    
-    def cm(self):
-        
-        cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
-        
-        if cycle == 2:
-            
-            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1) 
-            return self.max_m
-        
-        else:
-            x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
-            return self.max_m - (self.max_m-self.base_m)*np.maximum(0,(1-x))
-        
-        
-    def on_train_begin(self, logs={}):
-        logs = logs or {}
-
-        if self.clr_iterations == 0:
-            K.set_value(self.model.optimizer.lr, self.base_lr)
-        else:
-            K.set_value(self.model.optimizer.lr, self.clr())
-            
-        if self.cyclical_momentum == True:
-            if self.clr_iterations == 0:
-                K.set_value(self.model.optimizer.momentum, self.cm())
-            else:
-                K.set_value(self.model.optimizer.momentum, self.cm())
-            
-            
-    def on_batch_begin(self, batch, logs=None):
-        
-        logs = logs or {}
-        self.trn_iterations += 1
-        self.clr_iterations += 1
-
-        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
-        self.history.setdefault('iterations', []).append(self.trn_iterations)
-        
-        if self.cyclical_momentum == True:
-            self.history.setdefault('momentum', []).append(K.get_value(self.model.optimizer.momentum))
-
-        for k, v in logs.items():
-            self.history.setdefault(k, []).append(v)
-        
-        K.set_value(self.model.optimizer.lr, self.clr())
-        
-        if self.cyclical_momentum == True:
-            K.set_value(self.model.optimizer.momentum, self.cm())
-
-
-# ## Step 1 - обучение "головы"
-
-# Настройки
-batch_size = 1
-base_lr = LR_STEP1
-max_lr = base_lr*10
-max_m = MAX_MOMENTUM
-base_m = BASE_MOMENTUM
-
-cyclical_momentum = CYCLICAL_MOMENTUM
-augment = AUGMENT
-cycles = CYCLES
-
-# Расчет количества итерация и шага изменения learning rate
-iterations = round(train_generator.samples//train_generator.batch_size*epochs)
-iterations = list(range(0,iterations+1))
-step_size = len(iterations)/(cycles)
-
-model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=base_lr, momentum=BASE_MOMENTUM), metrics=["accuracy"])
-
-clr1 =  CyclicLR(
-    base_lr=LR_STEP1,
-    max_lr=LR_STEP1 * 10,
-    step_size=step_size,
-    max_m=MAX_MOMENTUM,
-    base_m=BASE_MOMENTUM,
-    cyclical_momentum=CYCLICAL_MOMENTUM
-)
-    
-# Добавим ModelCheckpoint чтоб сохранять прогресс обучения модели и можно было потом подгрузить и дообучить модель.    
-checkpoint = ModelCheckpoint('best_model.hdf5' , monitor = ['accuracy'] , verbose = 1  , mode = 'max')
-earlystop = EarlyStopping(monitor='accuracy', patience=5, restore_best_weights=True)
-
-# Обучаем
-history = model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples//train_generator.batch_size,
-    validation_data = test_generator, 
-    validation_steps = test_generator.samples//test_generator.batch_size,
-    epochs=EPOCHS_STEP1,
-    callbacks=[checkpoint, earlystop, clr1]
-    )
 
 scores = model.evaluate(test_generator, verbose=1)
-print("Accuracy: %.2f%%" % (scores[1]*100))
-
-# Сохраним итоговую сеть и подгрузим лучшую итерацию в обучении (best_model)
-model.save('../working/model_step1.hdf5')
-model.load_weights('best_model.hdf5')
-
-
-# ## Step 2 - FineTuning - обучение половины весов EfficientNetb6
-
-
-# Посмотрим на количество слоев в базовой модели
-print("Number of layers in the base model: ", len(base_model.layers))
-
-
-# Разморозим базовую модель
-base_model.trainable = True
-
-# Установим количество слоев, которые будем переобучать
-fine_tune_at = len(base_model.layers)//2
-
-# Заморозим первую половину слоев
-for layer in base_model.layers[:fine_tune_at]:
-    layer.trainable = False
-
-# Количество параметров
-len(base_model.trainable_variables)
-
-# Статус слоев - будем обучать или нет
-for layer in model.layers:
-    print(layer, layer.trainable)
-
-
-# Вновь обучаем модель, используя One Cycle Policy для Learning Rate
-
-# Расчет количества итерация и шага изменения learning rate
-iterations = round(train_generator.samples//train_generator.batch_size*epochs)
-iterations = list(range(0,iterations+1))
-step_size = len(iterations)/(cycles)
-
-model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=base_lr, momentum=BASE_MOMENTUM), metrics=["accuracy"])
-
-clr2 =  CyclicLR(
-    base_lr = LR_STEP2,
-    max_lr = LR_STEP2 * 10,
-    step_size = step_size,
-    max_m = MAX_MOMENTUM,
-    base_m = BASE_MOMENTUM,
-    cyclical_momentum = CYCLICAL_MOMENTUM
-)
-    
-# Обучаем
-history = model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples//train_generator.batch_size,
-    validation_data = test_generator, 
-    validation_steps = train_generator.samples//train_generator.batch_size,
-    epochs = EPOCHS_STEP2,
-    callbacks = [checkpoint, earlystop, clr2]
-)
-
-# Сохраним модель
-model.save('../working/model_step2.hdf5')
-model.load_weights('best_model.hdf5')
-
-
-scores = model.evaluate_generator(test_generator, verbose=1)
-print("Accuracy: %.2f%%" % (scores[1]*100))
-
-
-# ## Step 3 - FineTuning - разморозка всей сети EfficientNetB6 и дообучение
-
-# Разморозим базовую модель
-
-
-base_model.trainable = True
-
-
-# Повторно обучим модель, предварительно разморозив все слои
-
-
-# Настройки
-#batch_size = BATCH_SIZE
-epochs = EPOCHS_STEP3
-base_lr = LR_STEP3
-max_lr = base_lr*10
-#max_m = MAX_MOMENTUM
-#base_m = BASE_MOMENTUM
-
-#cyclical_momentum = CYCLICAL_MOMENTUM
-#augment = AUGMENT
-#cycles = CYCLES
-
-# Расчет количества итерация и шага изменения learning rate
-iterations = round(train_generator.samples // train_generator.batch_size * EPOCHS_STEP3)
-iterations = list(range(0, iterations+1))
-step_size = len(iterations) / (CYCLES)
-
-model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=base_lr, momentum=BASE_MOMENTUM), metrics=["accuracy"])
-
-clr3 =  CyclicLR(
-    base_lr = LR_STEP3,
-    max_lr = LR_STEP3 * 10,
-    step_size = step_size,
-    max_m = MAX_MOMENTUM,
-    base_m = BASE_MOMENTUM,
-    cyclical_momentum = CYCLICAL_MOMENTUM
-)
-    
-# Обучаем
-history = model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples//train_generator.batch_size,
-    validation_data = test_generator, 
-    validation_steps = train_generator.samples//train_generator.batch_size,
-    epochs = epochs,
-    callbacks = [checkpoint, earlystop, clr3]
-)
-
-
-model.save('../working/model_step3.hdf5')
-model.load_weights('best_model.hdf5')
-
-
-scores = model.evaluate_generator(test_generator, verbose=1)
 print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
@@ -475,7 +87,7 @@ print("Accuracy: %.2f%%" % (scores[1]*100))
 # Увеличим размер изображения и уменьшим уровень аугментации
 
 
-input_shape_step4 = (IMG_SIZE, IMG_SIZE, IMG_CHANNELS)
+input_shape_step4 = (IMG_SIZE_STEP4, IMG_SIZE_STEP4, IMG_CHANNELS)
 
 
 
@@ -508,7 +120,7 @@ test_datagen = ImageDataAugmentor(rescale=1./255)
 
 
 train_generator = train_datagen.flow_from_directory(
-    PATH+'train/',      # директория где расположены папки с картинками 
+    DATA_PATH+'train/',      # директория где расположены папки с картинками 
     target_size = (IMG_SIZE_STEP4, IMG_SIZE_STEP4),
     batch_size = BATCH_SIZE_STEP4,
     class_mode = 'categorical',
@@ -517,7 +129,7 @@ train_generator = train_datagen.flow_from_directory(
     subset = 'training') # set as training data
 
 test_generator = train_datagen.flow_from_directory(
-    PATH+'train/',
+    DATA_PATH+'train/',
     target_size = (IMG_SIZE_STEP4, IMG_SIZE_STEP4),
     batch_size = BATCH_SIZE_STEP4,
     class_mode = 'categorical',
@@ -598,7 +210,7 @@ from sklearn.metrics import accuracy_score
 
 test_sub_generator = test_datagen.flow_from_dataframe( 
     dataframe=sample_submission,
-    directory=PATH+'test_upload/',
+    directory=DATA_PATH+'test_upload/',
     x_col="Id",
     y_col=None,
     shuffle=False,
@@ -606,11 +218,6 @@ test_sub_generator = test_datagen.flow_from_dataframe(
     seed=RANDOM_SEED,
     target_size=(IMG_SIZE, IMG_SIZE),
     batch_size=BATCH_SIZE,)
-
-
-
-test_sub_generator.samples
-
 
 
 test_sub_generator.reset()
@@ -672,7 +279,7 @@ test_datagen = ImageDataAugmentor(
 
 test_sub_generator = test_datagen.flow_from_dataframe( 
     dataframe=sample_submission,
-    directory=PATH+'test_upload/',
+    directory=DATA_PATH+'test_upload/',
     x_col="Id",
     y_col=None,
     shuffle=False,
@@ -706,24 +313,4 @@ submission = pd.DataFrame({'Id':filenames_with_dir, 'Category':predictions}, col
 submission['Id'] = submission['Id'].replace('test_upload/','')
 submission.to_csv('submission_TTA.csv', index=False)
 print('Save submit')
-
-
-
-# Clean PATH
-import shutil
-shutil.rmtree(PATH)
-
-
-# # Итоги
-
-# В ходе выполнения проекта были применены следующие методы:
-# * transfer learning и fine-tuning
-# * настройка LR, optimizer, loss
-# * подобраны переменные (размер картинки, батч и т.д.)
-# * SOTA архитектура сетей - EfficientNetB6
-# * добавлена Batch Normalization и изменена архитектура “головы”
-# * применены дополнительные функции callback Keras https://keras.io/callbacks/ 
-# * TTA (Test Time Augmentation)
-# * Дополнительно*: разные техники управления Learning Rate (https://towardsdatascience.com/finding-good-learning-rate-and-the-one-cycle-policy-7159fe1db5d6 (eng) http://teleported.in/posts/cyclic-learning-rate/ (eng))
-# * Дополнительно*: использована более продвинутая библиотеки аугментации изображений (Albumentations )
 
